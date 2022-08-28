ac0703e3ffdea3abbb7a3f1c5f6b14f3932a4c44
Sator <lbvf1197@mail.ru>
2022-08-28 17:26:19 +0300

    add View Binding in Fragments

--- app/src/main/java/com/example/satcomposition/presentation/FragmentGame.kt
+++ app/src/main/java/com/example/satcomposition/presentation/FragmentGame.kt
@@ -1,6 +1,34 @@
 package com.example.satcomposition.presentation
 
+import android.os.Binder
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.satcomposition.R
+import com.example.satcomposition.databinding.FragmentGameBinding
+
 
 class FragmentGame: Fragment() {
+    private var _binding : FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding= FragmentGameBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
 }
--- app/src/main/java/com/example/satcomposition/presentation/FragmentGameResult.kt
+++ app/src/main/java/com/example/satcomposition/presentation/FragmentGameResult.kt
@@ -1,6 +1,32 @@
 package com.example.satcomposition.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.satcomposition.databinding.FragmentChooseLevelBinding
+import com.example.satcomposition.databinding.FragmentGameResultBinding
 
 class FragmentGameResult :Fragment() {
+    private var _binding : FragmentGameResultBinding? = null
+    private val binding: FragmentGameResultBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameResultBinding == null")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding= FragmentGameResultBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
 }
